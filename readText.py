7893e7df4783dd5930b2c68d4349688c8e75a028
darowebdeveloper <darowebdeveloper@gmail.com>
2022-09-08 21:15:32 +0700

    with comments docstring

--- readText.py
+++ readText.py
@@ -1,8 +1,23 @@
+"""Retrieve and print words from a URL.
+
+Usage:
+    python readText.py <URL>
+"""
+import sys
 from urllib.request import urlopen
 
 
-def fetch():
-    story = urlopen('http://sixty-north.com/c/t.txt')
+def fetch(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document.
+    """
+    # http://sixty-north.com/c/t.txt
+    story = urlopen(url)
     story_words = []
     for line in story:
         line_words = line.decode('utf-8').split()
@@ -11,9 +26,18 @@ def fetch():
 
     story.close()
 
-    for word in story_words:
+    return story_words
+
+
+def print_items(items):
+    for word in items:
         print(word)
 
 
+def main(url):
+    words = fetch(url)
+    print_items(words)
+
+
 if __name__ == '__main__':
-    fetch()
+    main(sys.argv[1])  # The 0th arg is the module filename
